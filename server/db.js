0242cc3da7cb37ca346d0ececca00dee1d5979e0
Michael Prather <mprather1@gmail.com>
2018-09-14 04:35:39 +0000

    production works

--- server/db.js
+++ server/db.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose')
 
 const DB = process.env['MONGO_INITDB_DATABASE']
 const URL = process.env['MONGO_URL']
-// const USERNAME = process.env['MONGO_INITDB_ROOT_USERNAME']
-// const PASSWORD = process.env['MONGO_INITDB_ROOT_PASSWORD']
 
 let connectionString = (process.env['MONGO_INITDB_DATABASE']) ? `mongodb://${URL}/${DB}` : 'mongodb://localhost/api_development'
 
